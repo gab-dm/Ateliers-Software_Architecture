c476e2549830101d4c76417d95e65242a46aba18
Gabriel Moreau de Montcheuil <78467767+gab-dm@users.noreply.github.com>
2021-05-03 11:13:50 +0200

    Init3

---
+++ CardShop-groupe9/src/main/java/com/project/CardShopgroupe9/CardShopGroupe9Application.java
@@ -0,0 +1,13 @@
+package com.project.CardShopgroupe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardShopGroupe9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardShopGroupe9Application.class, args);
+	}
+
+}
---
+++ CardShop-groupe9/src/test/java/com/project/CardShopgroupe9/CardShopGroupe9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CardShopgroupe9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardShopGroupe9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
