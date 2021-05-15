275f6ab500b4f9bfb72ce8fb257234b0c04f4030
uotab <thomas.laugel@cpe.fr>
2021-05-15 16:06:38 +0200

    Update Card.java

--- CardShop-groupe9/src/main/java/com/project/CardShopgroupe9/model/Card.java
+++ CardShop-groupe9/src/main/java/com/project/CardShopgroupe9/model/Card.java
@@ -13,16 +13,24 @@ public final class Card {
 private Integer id;
 private String name;
 private Integer hp;
-private String effect;
+private String description;
 private String imgUrl;
+private Integer attack;
+private Integer defence;
+private String family;
+private Integer energy;
 	
-	public Card ( String _name , Integer _hp, String effect, String imgUrl ) {
+	public Card ( String name , Integer hp, String description, String imgUrl, Integer attack, Integer defence, String family, Integer energy ) {
 		
 		super();
-		this.name = _name;
-		this.hp = _hp;
-		this.effect = effect ;
+		this.name = name;
+		this.hp = hp;
+		this.description = description ;
 		this.imgUrl = imgUrl; 
+		this.attack = attack;
+		this.defence = defence;
+		this.family = family;
+		this.energy = energy;
 		
 	}
 
@@ -50,12 +58,12 @@ private String imgUrl;
 		this.hp = hp;
 	}
 
-	public String getEffect() {
-		return effect;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setEffect(String effect) {
-		this.effect = effect;
+	public void setDescription(String description){
+		this.description = description;
 	}
 
 	public String getImgUrl() {
@@ -65,9 +73,41 @@ private String imgUrl;
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
+	
+	public void setAttack(Integer attack) {
+		this.attack = attack;
+	}
+	
+	public Integer getAttack() {
+		return attack;
+	}
+	
+	public void setDefence(Integer defence) {
+		this.defence = defence;
+	}
+	
+	public Integer getDefence() {
+		return defence;
+	}
+	
+	public void setEnergy(Integer energy) {
+		this.energy = energy;
+	}
+	
+	public Integer getEnergy() {
+		return energy;
+	}
+	public void setFamily(String family) {
+		this.family = family;
+	}
+	
+	public String getFamily() {
+		return family;
+	}
+	
 	@Override
 	public String toString() {
-		return "Carte ["+this.id+"]: name:"+this.name+", effet:"+this.effect +", hp:"+this.hp+" imgUrl:"+this.imgUrl;
+		return "Carte ["+getId()+"]: name:"+getName()+", family:"+getFamily() +", hp:"+getHp()+", imgUrl:"+getImgUrl() + ", attack:"+getAttack() + ", defence:" +getDefence() +", energy:"+getEnergy();
 	}
 
-}
+} 
